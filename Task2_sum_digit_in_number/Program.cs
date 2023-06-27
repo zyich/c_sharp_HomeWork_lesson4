31c39efc3d0a8167d57235f706598f11360963d0
zyich <zuilinho@yandex.ru>
2023-06-27 17:36:56 +0200

    task2

---
+++ Task2_sum_digit_in_number/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 27: Напишите программу, которая принимает
+ на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int GetNumber (string description)
+    {
+        int number;
+        Console.WriteLine($"{description} => ");
+        while (true)
+        {
+            string temp = Console.ReadLine();
+            if(int.TryParse(temp, out number))
+            {
+                return number;
+            }
+            Console.Write($"This number \"{temp}\" is not correct. Try again =>");
+        }
+    }
+
+int SumDigit (int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + (num%10);
+        num = num / 10;
+    }
+    return result;
+    }
+int number = GetNumber("input number");
+Console.WriteLine($"{SumDigit(number)}");
