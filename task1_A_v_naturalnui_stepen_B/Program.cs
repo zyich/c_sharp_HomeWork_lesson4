99e493456e8cde578b491af845de7c691bb9fab5
zyich <zuilinho@yandex.ru>
2023-06-27 16:18:13 +0200

    task 1

---
+++ task1_A_v_naturalnui_stepen_B/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите цикл, который принимает на
+ вход два числа (A и B) и возводит
+  число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+Console.WriteLine("Input First number = > ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Second number = > ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int MakeStepen = (int)Math.Pow(number1 , number2);
+
+Console.WriteLine($"при возведение {number1} в степень {number2} будет {MakeStepen}");
+
+
+
+
+
